16edbf54bc8645a05958c0936e0a87950fe324f8
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-06-17 14:53:08 -0300

    style: Add global styles to the project.

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyle = createGlobalStyle`
+  * {
+    border: 0;
+    padding: 0;
+    margin: 0;
+    font-family: 'Exo', sans-serif;
+  }
+
+  body{
+    background-color: #063940;
+  }
+`;
---
+++ src/styles/Theme.tsx
@@ -0,0 +1,26 @@
+export const Theme = {
+  colors: {
+    primary: "#3e838c",
+    secondary: "#195e63",
+    tertiary: "#8ebdb6",
+    background: "#063940",
+    text: "#ece1c3",
+    textLight: "#FFFFFF",
+    disabledText: "#d3d3d3",
+  },
+  fonts: {
+    title: "Barlow Condensed, sans-serif",
+    text: "Exo, sans-serif",
+  },
+  fontSizes: {
+    small: "14px",
+    medium: "24px",
+    large: "48px",
+    big: "82px",
+  },
+  spacing: {
+    small: "8px",
+    medium: "16px",
+    large: "24px",
+  },
+};
